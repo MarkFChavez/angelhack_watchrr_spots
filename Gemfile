f92771f1ad7cea4b6f130765f67e2133fc70d3eb
Mark <markchav3z@gmail.com>
2016-04-23 20:02:27 +0800

    Enable cross origin request.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'google_places'
 gem 'byebug'
+gem 'sinatra-cross_origin', '~> 0.3.1'
--- angelhack_watchrr.rb
+++ angelhack_watchrr.rb
@@ -1,15 +1,24 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+
 require 'json'
 require 'google_places'
+
 require 'byebug'
 
 API_KEY = 'AIzaSyCL9LZPG85foCDceeIs_pNHD4SfK8__7vk'
 
+configure do
+  enable :cross_origin
+end
+
 get "/search" do
+  return "No `key` parameter is provided."
+
   ary = []
 
   spots = client.spots_by_query(params[:key])
-  spots.each do |spot|
+  spots.map do |spot|
     ary << build_spot(spot)
   end
 
@@ -25,6 +34,7 @@ def build_spot(spot)
     name: spot.name,
     formatted_address: spot.formatted_address,
     lat: spot.lat,
-    lng: spot.lng
+    lng: spot.lng,
+    icon: spot.icon
   }
 end
